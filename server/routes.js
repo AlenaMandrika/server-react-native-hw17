522ad3f0373606c9380e0f60940c31ea688e9370
alenamandrika <Alenenok0@ukr.net>
2018-03-08 00:27:10 +0200

    amended POST, PUT

--- server/routes.js
+++ server/routes.js
@@ -21,34 +21,39 @@ router.get('/user', (req, res, next) => {
     .catch(next)
 })
 
-router.post('/user', (req, res, next) => {
+router.post('/user', (req, res) => {
   let newUser = new User(req.body.user)
-  if (req.body.user.email === '' || req.body.user.password === '' || req.body.user.username === '') {
-    console.log('user err , field is empty : ', req.body.user)
-
-    res.json({success: false, message: 'Please fill in the fields'})
-  } else {
+  if (req.body.user.email !== '' && req.body.user.password !== '' && req.body.user.username !== '') {
     newUser.save()
       .then(user => {
         res.json({user})
+      }, (err) => {
+        res.status(400).json(err)
       })
-      .catch(next)
+  } else {
+    console.log('user err , field is empty : ', req.body.user)
+    res.status(400).json({success: false, message: 'Please fill in the fields'})
   }
 })
 
 router.put('/user/:id', function (req, res) {
-  User.findOneAndUpdate({'_id': req.params.id},
-    {email: req.body.user.email,
-      password: req.body.user.password,
-      username: req.body.user.username
-    },
-    {new: true},
-    function (err, user) {
-      if (err) {
-        res.status(400).json(err)
-      }
-      res.status(200).json(user)
-    })
+  let user = req.body.user
+  if (('email' in user) && ('password' in user) && ('username' in user)) {
+    User.findOneAndUpdate({'_id': req.params.id},
+      {email: req.body.user.email,
+        password: req.body.user.password,
+        username: req.body.user.username
+      },
+      {new: true},
+      function (err, user) {
+        if (err) {
+          res.status(400).json(err)
+        }
+        res.status(200).json(user)
+      })
+  } else {
+    res.status(400).json({success: false, message: 'unacceptable changes'})
+  }
 })
 
 router.delete('/user/:id', function (req, res) {
